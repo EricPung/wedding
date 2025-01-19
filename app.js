63659ec45576703c1ab38cd1a1d357a6a490874d
eric <ericpung@foxmail.com>
2025-01-19 22:55:09 +0800

    初始化

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 // app.js
 App({
-  onLaunch() {
-
-    // 展示本地存储能力
-    // const logs = wx.getStorageSync('logs') || []
-    // logs.unshift(Date.now())
-    // wx.setStorageSync('logs', logs)
-
-    // 登录
+  onLaunch({
+    query
+  }) {
+    let city = wx.getStorageSync('city')
+    if (!city) {
+      wx.setStorageSync('city', "cd")
+    }
+    if (query.city) {
+      wx.setStorageSync('city', query.city)
+    }
     wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      success(res) {
+        if (res.code) {
+          //发起网络请求
+          wx.request({
+            url: 'https://www.ruida.studio:8090/verify',
+            data: {
+              code: res.code
+            },
+            success({
+              data
+            }) {
+              // console.log(data)
+              if (data && data.status === "allow") {
+                wx.setStorageSync('share', 'allow')
+              } else {
+                wx.setStorageSync('share', 'error')
+              }
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
       }
     })
   },
